a9650809342148f69a1698c99420345b0b33005f
Natalie <natalie@cern.ch>
2015-03-16 19:34:34 +0100

    final changes - compiles

---
+++ RecoLocalTracker/SiPixelStubBuilder/plugins/SealModules.cc
@@ -0,0 +1,9 @@
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "RecoLocalTracker/SiPixelStubBuilder/interface/SiPixelStubProducer.h"
+//
+using cms::SiPixelStubProducer;
+
+DEFINE_FWK_MODULE(SiPixelStubProducer);
+
--- RecoLocalTracker/SiPixelStubBuilder/plugins/module.cc
+++
@@ -1,9 +0,0 @@
-
-#include "DataFormats/SiPixelStub/interface/SiPixelStub.h"
-
-
-/// The Stub Finding Algorithms
-#include "RecoLocalTracker/SiPixelStubBuilder/interface/SiPixelStubAlgorithm_test.h"
-//DEFINE_FWK_EVENTSETUP_MODULE(SiPixelStubAlgorithm_test);
-
-
