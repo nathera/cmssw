b6424546aed5277fff0b6ddd008ac4089f9b77dd
Natalie <natalie@cern.ch>
2015-03-18 11:06:12 +0100

    created SiPixelStubBuilder pkg

---
+++ RecoLocalTracker/SiPixelStubBuilder/interface/DummyStubBuilder.h
@@ -0,0 +1,76 @@
+#ifndef RecoLocalTracker_SiStubBuilder_DummyStubBuilder_H
+#define RecoLocalTracker_SiStubBuilder_DummyStubBuilder_H
+
+//-----------------------------------------------------------------------
+//! \class DummyStubBuilder
+//! \brief An dummy stub building algorithm
+//!
+
+//-----------------------------------------------------------------------
+
+// Base class, defines SiPixelDigi and SiPixelCluster.  The latter includes
+// Pixel, PixelPos and Shift as inner classes.
+//
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "RecoLocalTracker/SiPixelStubBuilder/interface/SiPixelStubBuilderBase.h"
+
+// The private pixel buffer
+#include "RecoLocalTracker/SiPixelStubBuilder/interface/SiPixelArrayBuffer.h"
+
+// Parameter Set:
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include <vector>
+
+
+class DummyStubBuilder : public SiPixelStubBuilderBase {
+ public:
+
+  DummyStubBuilder(edm::ParameterSet const& conf);
+  ~DummyStubBuilder();
+
+  // Full I/O in DetSet
+  void buildDetUnit( const edm::DetSet<SiPixelCluster> & input, 
+                     const PixelGeomDetUnit * pixDet,
+                     edmNew::DetSetVector<SiPixelStub>::FastFiller& output);
+
+  
+ private:
+
+  edm::ParameterSet conf_;
+
+  //! Data storage
+  SiPixelArrayBuffer               theBuffer;         // internal nrow * ncol matrix
+  bool                             bufferAlreadySet;  // status of the buffer array
+  std::vector<SiPixelCluster::PixelPos>  theSeeds;          // cached seed pixels
+  std::vector<SiPixelCluster>            theClusters;       // resulting clusters  
+  
+  //! Clustering-related quantities:
+  float thePixelThresholdInNoiseUnits;    // Pixel threshold in units of noise
+  float theSeedThresholdInNoiseUnits;     // Pixel cluster seed in units of noise
+  float theClusterThresholdInNoiseUnits;  // Cluster threshold in units of noise
+
+  int   thePixelThreshold;  // Pixel threshold in electrons
+  int   theSeedThreshold;   // Seed threshold in electrons 
+  float theClusterThreshold;  // Cluster threshold in electrons
+  int   theConversionFactor;  // adc to electron conversion factor
+  int   theOffset;            // adc to electron conversion offset
+
+  //! Geometry-related information
+  int  theNumOfRows;
+  int  theNumOfCols;
+  uint32_t detid_;
+  bool dead_flag;
+  bool doMissCalibrate; // Use calibration or not
+  bool doSplitClusters;
+  //! Private helper methods:
+  bool setup(const PixelGeomDetUnit * pixDet);
+
+  // Calibrate the ADC charge to electrons 
+  int   theStackADC_;          // The maximum ADC count for the stack layers
+  int   theFirstStack_;        // The index of the first stack layer
+
+
+};
+
+#endif
---
+++ RecoLocalTracker/SiPixelStubBuilder/src/DummyStubBuilder.cc
@@ -0,0 +1,109 @@
+//----------------------------------------------------------------------------
+//! \class DummyStubBuilder
+//! \brief A specific threshold-based pixel clustering algorithm
+//!
+
+//----------------------------------------------------------------------------
+
+// Our own includes
+#include "RecoLocalTracker/SiPixelStubBuilder/interface/DummyStubBuilder.h"
+#include "RecoLocalTracker/SiPixelStubBuilder/interface/SiPixelArrayBuffer.h"
+#include "CondFormats/SiPixelObjects/interface/SiPixelGainCalibrationOffline.h"
+// Geometry
+#include "Geometry/TrackerGeometryBuilder/interface/PixelGeomDetUnit.h"
+#include "Geometry/CommonTopologies/interface/PixelTopology.h"
+//#include "Geometry/CommonTopologies/RectangularPixelTopology.h"
+#include "DataFormats/SiPixelDetId/interface/PXBDetId.h"
+
+// STL
+#include <stack>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+//----------------------------------------------------------------------------
+//! Constructor: 
+//!  Initilize the buffer to hold pixels from a detector module.
+//!  This is a vector of 44k ints, stays valid all the time.  
+//----------------------------------------------------------------------------
+DummyStubBuilder::DummyStubBuilder
+  (edm::ParameterSet const& conf) :
+    conf_(conf), bufferAlreadySet(false), theNumOfRows(0), theNumOfCols(0), detid_(0) 
+{
+  // Get thresholds in electrons
+  thePixelThreshold   = 
+    conf_.getParameter<int>("ChannelThreshold");
+  theSeedThreshold    = 
+    conf_.getParameter<int>("SeedThreshold");
+  theClusterThreshold = 
+    conf_.getParameter<double>("ClusterThreshold");
+  theConversionFactor = 
+    conf_.getParameter<int>("VCaltoElectronGain");
+  theOffset = 
+    conf_.getParameter<int>("VCaltoElectronOffset");
+  if ( conf_.exists("AdcFullScaleStack") ) theStackADC_=conf_.getParameter<int>("AdcFullScaleStack");
+  else 
+    theStackADC_=255;
+  if ( conf_.exists("FirstStackLayer") ) theFirstStack_=conf_.getParameter<int>("FirstStackLayer");
+  else
+    theFirstStack_=5;
+  
+  // Get the constants for the miss-calibration studies
+  doMissCalibrate=conf_.getUntrackedParameter<bool>("MissCalibrate",true); 
+  doSplitClusters = conf.getParameter<bool>("SplitClusters");
+  theBuffer.setSize( theNumOfRows, theNumOfCols );
+}
+/////////////////////////////////////////////////////////////////////////////
+DummyStubBuilder::~DummyStubBuilder() {}
+
+//----------------------------------------------------------------------------
+//!  Prepare the Clusterizer to work on a particular DetUnit.  Re-init the
+//!  size of the panel/plaquette (so update nrows and ncols), 
+//----------------------------------------------------------------------------
+bool DummyStubBuilder::setup(const PixelGeomDetUnit * pixDet) 
+{
+  // Cache the topology.
+  const PixelTopology & topol = pixDet->specificTopology();
+  
+  // Get the new sizes.
+  int nrows = topol.nrows();      // rows in x
+  int ncols = topol.ncolumns();   // cols in y
+  
+  theNumOfRows = nrows;  // Set new sizes
+  theNumOfCols = ncols;
+  
+  if ( nrows > theBuffer.rows() || 
+       ncols > theBuffer.columns() ) 
+    { // change only when a larger is needed
+      //if( nrows != theNumOfRows || ncols != theNumOfCols ) {
+      //cout << " DummyStubBuilder: pixel buffer redefined to " 
+      // << nrows << " * " << ncols << endl;      
+      //theNumOfRows = nrows;  // Set new sizes
+      //theNumOfCols = ncols;
+      // Resize the buffer
+      theBuffer.setSize(nrows,ncols);  // Modify
+      bufferAlreadySet = true;
+    }
+  
+  return true;   
+}
+//----------------------------------------------------------------------------
+//!  \brief Cluster pixels.
+//!  This method operates on a matrix of pixels
+//!  and finds the largest contiguous cluster around
+//!  each seed pixel.
+//!  Input and output data stored in DetSet
+//----------------------------------------------------------------------------
+void DummyStubBuilder::buildDetUnit( const edm::DetSet<SiPixelCluster> & input, 
+                                     const PixelGeomDetUnit * pixDet,
+                                     edmNew::DetSetVector<SiPixelStub>::FastFiller& output)  {
+  
+//   SiClusterIterator begin = input.begin();
+//   SiClusterIterator end   = input.end();
+  
+  
+  
+}
+
+
+
